18a0d07362f7c49e8333d29fa2097d4085a7bcaa
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-05-29 11:12:01 +0530

    Create CustomHtmlHelpers.cs

---
+++ Day 17 Assignment A/Helpers/CustomHtmlHelpers.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CustomerFeedbackPortal.Helpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlContent StyledInput(this IHtmlHelper htmlHelper, string name, string placeholder)
+        {
+            string input = $"<input type='text' name='{name}' placeholder='{placeholder}' class='form-control mb-3' />";
+
+            return new HtmlString(input);
+        }
+    }
+}
