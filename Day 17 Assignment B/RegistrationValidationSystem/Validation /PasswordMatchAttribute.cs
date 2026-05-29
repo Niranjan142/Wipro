5940cca8b369e7a0297b090dd63c4cae98af46c3
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-05-29 11:32:50 +0530

    Create PasswordMatchAttribute.cs

---
+++ Day 17 Assignment B/RegistrationValidationSystem/Validation /PasswordMatchAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationValidationSystem.Validation
+{
+    public class PasswordMatchAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid
+        (
+            object value,
+            ValidationContext validationContext
+        )
+        {
+            var user =
+                (Models.UserRegistration)
+                validationContext.ObjectInstance;
+
+            if (user.Password != user.ConfirmPassword)
+            {
+                return new ValidationResult(
+                    "Password and Confirm Password do not match"
+                );
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
