977eba9c7d0bc429d0e4da1a392955e11dda9d33
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-05-29 11:15:13 +0530

    Create RatingTagHelper.cs

---
+++ Day 17 Assignment A/TagHelpers /RatingTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomerFeedbackPortal.TagHelpers
+{
+    [HtmlTargetElement("rating-stars")]
+    public class RatingTagHelper : TagHelper
+    {
+        public int Count { get; set; } = 5;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+
+            string stars = "";
+
+            for (int i = 1; i <= Count; i++)
+            {
+                stars += $"<span style='font-size:25px;color:gold;'>★</span>";
+            }
+
+            output.Content.SetHtmlContent(stars);
+        }
+    }
+}
