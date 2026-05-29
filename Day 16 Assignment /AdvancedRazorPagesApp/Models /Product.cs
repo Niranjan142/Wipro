e58daccd0b01376a93d4372457ac7c4351e7991c
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-05-29 11:44:11 +0530

    Create Product.cs

---
+++ Day 16 Assignment /AdvancedRazorPagesApp/Models /Product.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedRazorPagesApp.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        // Collection of categories
+        public List<Category> Categories { get; set; }
+            = new List<Category>();
+    }
+}
