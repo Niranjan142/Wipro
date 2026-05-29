1d5ed670fa90dbe923a06fc02590088c3eb49ff6
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-05-29 11:26:51 +0530

    Create UserRegistration.cs

---
+++ Day 17 Assignment B/RegistrationValidationSystem/Models /UserRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RegistrationValidationSystem.Validation;
+
+namespace RegistrationValidationSystem.Models
+{
+    public class UserRegistration
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [PasswordMatch]
+        public string ConfirmPassword { get; set; }
+    }
+}
