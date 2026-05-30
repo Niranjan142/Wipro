f9331cfeb87e052d64cd61cd81fdf9acb3f12dd1
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-05-30 19:44:53 +0530

    Create ItemRepository.cs

---
+++ Practice Assignment - .Net Core /Assignment 1: Building a .NET Core Application with Middleware/Models/ItemRepository.cs
@@ -0,0 +1,10 @@
+public static class ItemRepository
+{
+    public static List<string> Items { get; set; } =
+        new List<string>
+        {
+            "Laptop",
+            "Mobile",
+            "Keyboard"
+        };
+}
