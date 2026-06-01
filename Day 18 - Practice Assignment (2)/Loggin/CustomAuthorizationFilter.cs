2dce511f95254d37a5ab9dc289106aaa7488c64d
Niranjan142 <104497199+Niranjan142@users.noreply.github.com>
2026-06-01 13:21:43 +0530

    Create CustomAuthorizationFilter.cs

---
+++ Day 18 - Practice Assignment (2)/Loggin/CustomAuthorizationFilter.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class CustomAuthorizationFilter : Attribute, IAuthorizationFilter
+{
+    private readonly string _role;
+
+    public CustomAuthorizationFilter(string role)
+    {
+        _role = role;
+    }
+
+    public void OnAuthorization(
+        AuthorizationFilterContext context)
+    {
+        var userRole =
+            context.HttpContext.Session.GetString("Role");
+
+        if (userRole != _role)
+        {
+            context.Result =
+                new ForbidResult();
+        }
+    }
+}
